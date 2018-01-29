4f7c287e4c94fb2960545992553cde7e5d0b3b73
Harious <harious@HariousdeiMac.local>
2018-01-29 15:07:09 +0800

    update

--- BlockDemo/BlockVlaueCaptureViewController.m
+++ BlockDemo/BlockVlaueCaptureViewController.m
@@ -1,21 +1,28 @@
 
 
 /*
- 1.对于局部变量的值捕获
- block外部没有加__block的基本类型，block内变量的值取决于block实现的时候，而不是block调用的时候
- block外部加了__block的基本类型，block内变量的值取决于block调用的时间
  
- block外部没有加__block的对象，block内对象取决于block实现的时候，而不是block调用的时候（类似基本类型）；但是对象属性的值不受这个限制，取决于调用block的时间
- block外部加了__block的对象，block内对象及其属性都取决于block调用时间
- 对于NSMutableDictionary、NSMutableArray、NSMutableString等可变类型,不加__block的情况，对其的可变操作类似对象的属性操作,取决于取决于block调用时间
+一、值捕获
+ 
+    1.对于局部变量的值捕获
+    block外部没有加__block的基本类型，block内变量的值取决于block实现的时候，而不是block调用的时候
+    block外部加了__block的基本类型，block内变量的值取决于block调用的时间
+ 
+    block外部没有加__block的对象，block内对象取决于block实现的时候，而不是block调用的时候（类似基本类型）；但是对象属性的值不受这个限制，取决于调用block的时间
+    block外部加了__block的对象，block内对象及其属性都取决于block调用时间
+    对于NSMutableDictionary、NSMutableArray、NSMutableString等可变类型,不加__block的情况，对其的可变操作类似对象的属性操作,取决于取决于block调用时间
+ 
+    2.对属性及实例变量的捕获
+    取决于block调用时间
+ 
+    3.对全局变量的捕获
+    取决于block调用时间
  
  
- 2.对属性及实例变量的捕获
- 取决于block调用时间
+ 二、block内部修改block的变量
  
- 
- 3.对全局变量的捕获
- 取决于block调用时间
+    block内部不允许修改block外部没有被__block修饰的局部变量，对于可变类型（如NSMutableString等）做出的可变操作是允许而且有效的
+    全局变量和实例变量及属性不受影响
  */
 
 #import "BlockVlaueCaptureViewController.h"
@@ -42,10 +49,11 @@ NSString *globleString = @"000";
     
 //    [self capturelocalVariable1];
     
-    
 //    [self captureProperty];
     
-    [self captureGloble];
+//    [self captureGloble];
+    
+    [self blockInsideModifyVar];
 }
 
 /// block 获取局部变量
@@ -146,6 +154,30 @@ NSString *globleString = @"000";
     block();
 }
 
+/// block内部修改变量
+- (void)blockInsideModifyVar {
+    
+    NSMutableArray *arr = [NSMutableArray arrayWithArray:@[@"1"]];
+    NSInteger index = 5;
+    __block NSInteger blockIndex = 5;
+    
+    void (^block)(void) = ^() {
+//        index = 10;
+        [arr addObject:@"2"];
+        blockIndex = 10;
+        _number = 999;
+        globleString = @"888888";
+    };
+    
+    NSLog(@"%zd    %zd    %zd   %@", index, blockIndex, _number, arr);
+    
+    block();
+    
+    NSLog(@"%zd    %zd    %zd   %@", index, blockIndex, _number, arr);
+    
+    NSLog(@"%@", globleString);
+    
+}
 
 @end
 
